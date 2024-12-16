f158968dc57e1efd427d90f36c2f21b936e28a85
Garin Caesar <garincaesar69@gmail.com>
2024-12-16 15:12:24 +0700

    feat: scanner and input handler

---
+++ src/com/enigmacamp/NasabahInputHandler.java
@@ -0,0 +1,30 @@
+package com.enigmacamp;
+
+import java.util.Scanner;
+
+public class NasabahInputHandler {
+    private Scanner scanner;
+
+    public NasabahInputHandler(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int getInt(String prompt){
+        System.out.print(prompt);
+
+        while(!scanner.hasNextInt()){
+            System.out.println("Input harus berupa angka !");
+            System.out.println();
+            scanner.nextLine();
+            System.out.print(prompt);
+        }
+        int result = scanner.nextInt();
+        scanner.nextLine();
+        return result;
+    }
+
+    public String getString(String prompt){
+        System.out.print(prompt);
+        return scanner.nextLine();
+    }
+}
